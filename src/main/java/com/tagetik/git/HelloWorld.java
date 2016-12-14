884667231b7bc35c56bfa16a58026f362a72eb7c
Matteo Betti <matteob@grupposervizi.it>
2016-12-14 10:09:15 +0100

    pippo

--- src/main/java/com/tagetik/git/HelloWorld.java
+++ src/main/java/com/tagetik/git/HelloWorld.java
@@ -4,4 +4,7 @@ package com.tagetik.git;
  * Created by matteob on 12/12/2016.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("plutop");
+    }
 }
--- src/main/java/com/tagetik/git/utils/Utils.java
+++ src/main/java/com/tagetik/git/utils/Utils.java
@@ -4,4 +4,9 @@ package com.tagetik.git.utils;
  * Created by matteob on 12/12/2016.
  */
 public class Utils {
+
+    public static void main(String[] args) {
+        System.out.println("pippop");
+
+    }
 }
